347ef231cbafe2238c4eca7071ab3466559c5b1f
tannervanes <tannervanes@gmail.com>
2023-11-12 13:06:38 -0700

    created a source and .h file

---
+++ Test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by tannervanes on 11/12/23.
+//
+
+#include "Test.h"
+#include <iostream>
+int main(){
+    std::cout << "Hello";
+    return 0;
+}
---
+++ Test.h
@@ -0,0 +1,8 @@
+//
+// Created by tannervanes on 11/12/23.
+//
+
+#ifndef CLASSPROJECT_TEST_H
+#define CLASSPROJECT_TEST_H
+
+#endif //CLASSPROJECT_TEST_H
