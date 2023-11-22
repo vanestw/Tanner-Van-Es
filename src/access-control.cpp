460b3e438ac6a537abf44cda0e73a87251ab14a4
Eli <yellowclam@proton.me>
2023-11-21 17:00:38 -0700

    Added a few comments and actually added the type for the iterator

--- src/access-control.cpp
+++ src/access-control.cpp
@@ -12,15 +12,16 @@ AccessControl::AccessControl(const string& user, const string& password) {
 
 bool AccessControl::CheckLogin(const string& username, const string& password) {
     // Checking if provided credentials match any role
-    auto it = roles.find(username);
-    if(it != roles.end() && it->second == password) {
+    map<string, string>::iterator it = roles.find(username); // setting iterator 'it' to username
+    if(it != roles.end() && it->second == password) { // checks if username and password combo match
         // Set the current role based on the username
         role = it->first;
         return true;
     }
-    return false;
+    return false; // returns false if no match
 }
 
+// Returns current role
 string AccessControl::GetCurrentRole() const {
 	return role;
 }
