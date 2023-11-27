aff766c4099aa7e6fb583473a2168ac93930e674
Eli <yellowclam@proton.me>
2023-11-27 12:44:58 -0700

    Merging all PRs

--- SaleInfo.cpp
+++ SaleInfo.cpp
@@ -1,3 +1,17 @@
+#include <iostream>
+#include "SaleInfo.h"
+using namespace std;
+
 void SaleInfo::PrintMonthSales() {
 	cout << salesByMonth << endl;
+}
+
+void printDaySales() {
+	setSalesByDay(int daySales);
+	cout << "Here are the sales for the day: " << salesByDay << endl;
+}
+void printWeekBySales(int salesByWeek) {
+
+cout << "Sales for this week: " << salesByWeek << endl;
+
 }
